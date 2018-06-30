b37bea54e9583b00691b1a4d570c497548101a43
bianca longhi <longhisbianca@gmail.com>
2018-06-30 18:34:17 -0300

    Implementando classe ListagemCandidato

--- app/src/main/java/br/edu/iff/pooa20181/trabalho02_2018_1/ListagemCandidatoActivity.java
+++ app/src/main/java/br/edu/iff/pooa20181/trabalho02_2018_1/ListagemCandidatoActivity.java
@@ -1,14 +1,25 @@
 package br.edu.iff.pooa20181.trabalho02_2018_1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class ListagemCandidatoActivity extends AppCompatActivity {
+import java.util.List;
 
+import br.edu.iff.pooa20181.trabalho02_2018_1.adapter.CandidatoAdapter;
+import br.edu.iff.pooa20181.trabalho02_2018_1.adapter.ClickRecyclerViewListener;
+import br.edu.iff.pooa20181.trabalho02_2018_1.model.Candidato;
+import io.realm.Realm;
+
+public class ListagemCandidatoActivity extends AppCompatActivity implements ClickRecyclerViewListener {
+
+    private Realm realm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +31,42 @@ public class ListagemCandidatoActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(ListagemCandidatoActivity.this,CandidatoDetalhe.class);
+                //id passado para a pagina de salvar,editar e criar candidato
+                intent.putExtra("id",0);
+                startActivity(intent);
             }
         });
+    }
+
+    protected void onResume() {
+
+        super.onResume();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.lista_candidato);
+
+        recyclerView.setAdapter(new CandidatoAdapter(getEventos(),this,this));
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    public List<Candidato> getEventos(){
+
+        return (List) realm.where(Candidato.class).findAll();
+
+    }
+
+    @Override
+    public void onClick(Object object) {
+        Candidato candidato = (Candidato) object;
+        Intent intent = new Intent(ListagemCandidatoActivity.this,CandidatoDetalhe.class);
+        intent.putExtra("id",candidato.getId());
+        startActivity(intent);
+    }
+
+    public void finish(){
+        super.finish();
+        realm.close();
     }
 
 }
